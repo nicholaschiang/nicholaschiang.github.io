dfd17ee2a7051d64767d1fb2f9d26fdc9d3cfe32
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2019-07-23 15:29:11 -0700

    feat: move videos into mdc-layout-grid

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,15 @@
 import Plyr from 'plyr';
 
-const player = new Plyr('#player');
-player.muted = true;
-player.loop = true;
-player.autoplay = true;
+var players = [];
+document.querySelectorAll('#player').forEach((el) => {
+    var player = new Plyr(el);
+    players.push(player);
+});
 
 window.onload = () => {
+    var player = players[0];
+    player.muted = true;
+    player.loop = true;
+    player.autoplay = true;
     player.play();
 };
