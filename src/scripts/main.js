2deab329131938543b53aeea87adb087608b8f56
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2019-07-23 15:25:23 -0700

    feat: add video to homepage

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
-// stub
+import Plyr from 'plyr';
+
+const player = new Plyr('#player');
+player.muted = true;
+player.loop = true;
+player.autoplay = true;
+
+window.onload = () => {
+    player.play();
+};
